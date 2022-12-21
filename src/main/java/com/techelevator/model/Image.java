f226d7306a1c14f58f1a8c0b0892e043408498d8
RicardoCruz21 <abstergo21@gmail.com>
2022-12-20 20:01:59 -0600

    create model for Image

---
+++ src/main/java/com/techelevator/model/Image.java
@@ -0,0 +1,53 @@
+package com.techelevator.model;
+
+public class Image {
+
+    private int id;
+    private String name;
+    private int size;
+    private int type;
+    private byte[] content;
+
+    public Image() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
